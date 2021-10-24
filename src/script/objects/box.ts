03494e309d504741f4ba0a7ff1f777ff8e90448f
William Owen <william.owen.bsc@gmail.com>
2021-10-24 15:44:15 -0700

    filling in box... basic set up

--- src/script/objects/box.ts
+++ src/script/objects/box.ts
@@ -1,6 +1,34 @@
 export default class Box {
+  containerEl: HTMLDivElement;
   constructor(public type: string) {
-    console.log('box made', this.type);
+    this.containerEl = document.querySelector(`.box.${this.type}`)
+    console.log('box made', this.type, this.containerEl);
+
+    this.configure();
+  }
+
+  setTime = () => {
+
+  }
+
+  insertEls = (el1: HTMLDivElement, el2: HTMLDivElement) => {
+    this.containerEl.insertAdjacentElement('afterbegin', el1)
+    this.containerEl.insertAdjacentElement('beforeend', el2)
+  }
+
+  createEls = () => {
+    const topDiv = document.createElement('div')
+    topDiv.setAttribute('class', 'top');
+    const bottomDiv = document.createElement('div')
+    bottomDiv.setAttribute('class', 'bottom');
+    
+    this.insertEls(topDiv, bottomDiv)
+  }
+  
+  configure = () => {
+    this.createEls();
+
+    this.setTime();
     
   }
 }
