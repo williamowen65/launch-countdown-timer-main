cbb8203c11e9e292c128e718f20a5593de8c7b2e
William Owen <william.owen.bsc@gmail.com>
2021-10-24 20:07:54 -0700

    better flip

--- src/script/objects/box.ts
+++ src/script/objects/box.ts
@@ -7,6 +7,8 @@ export default class Box {
   bottom: HTMLDivElement;
   topNum: HTMLDivElement;
   bottomNum: HTMLDivElement;
+  topNext: HTMLDivElement;
+  topNextNum: HTMLDivElement;
   constructor(public type: string) {
     this.containerEl = document.querySelector(`.box.${this.type}`)
     console.log('box made', this.type, this.containerEl);
@@ -16,8 +18,10 @@ export default class Box {
 
   flip = () => {
     this.top.setAttribute('class','top flip')
+    // this.topNext.setAttribute('class','top next show')
     setTimeout(() => {
       this.top.setAttribute('class', 'top')
+      // this.topNext.setAttribute('class','top next')
     },500)
   }
 
@@ -40,10 +44,16 @@ export default class Box {
         break;
     }
 
-   if(this.type == 'seconds'){
-     this.flip()
-   }
-
+  //  if(this.type == 'seconds'){
+  //    this.flip()
+  //  }
+  console.log(this.topNext);
+  
+  if(typeTime === 0) {
+    this.topNext.innerText = `59`
+  } else {
+    this.topNext.innerText = `${typeTime - 1}`
+  }
     this.topNum.innerText = '' + typeTime
     this.bottomNum.innerText = '' + typeTime
   
@@ -62,36 +72,52 @@ export default class Box {
    
     setInterval(() => {
       this.timeLeft = this.timeLeft.minus({seconds: 1})
-     
-      this.printTime();
+        if(this.type == 'seconds'){
+          // this.flip()
+        }
+      // setTimeout(() => {
+        // this.printTime();
+      // },1)
     }, 1000)
   }
 
-  insertEls = (el1: HTMLDivElement, el2: HTMLDivElement) => {
+  insertEls = (el1: HTMLDivElement, el2: HTMLDivElement, el3: HTMLDivElement) => {
     this.containerEl.insertAdjacentElement('afterbegin', el1)
+    this.containerEl.insertAdjacentElement('afterbegin', el3)
     this.containerEl.insertAdjacentElement('beforeend', el2)
 
     const top = this.containerEl.querySelector('.top')
+    const topNext = this.containerEl.querySelector('.next')
     const bottom = this.containerEl.querySelector('.bottom')
+
     const timeDiv = document.createElement('div')
     timeDiv.setAttribute('class','time')
+    top.insertAdjacentElement('beforeend', timeDiv)
+
+    const timeDiv3 = document.createElement('div')
+    timeDiv3.setAttribute('class','time')
+    topNext.insertAdjacentElement('beforeend', timeDiv3)
+
     const timeDiv2 = document.createElement('div')
     timeDiv2.setAttribute('class','time')
-    top.insertAdjacentElement('beforeend', timeDiv)
     bottom.insertAdjacentElement('beforeend', timeDiv2)
+
     this.topNum = this.containerEl.querySelector('.top .time')
     this.bottomNum = this.containerEl.querySelector('.bottom .time')
     this.top = this.containerEl.querySelector('.top')
+    this.topNext = this.containerEl.querySelector('.next .time')
     this.bottom = this.containerEl.querySelector('.bottom')
   }
 
   createEls = () => {
     const topDiv = document.createElement('div')
     topDiv.setAttribute('class', 'top');
+    const topNextDiv = document.createElement('div')
+    topNextDiv.setAttribute('class', 'next');
     const bottomDiv = document.createElement('div')
     bottomDiv.setAttribute('class', 'bottom');
     
-    this.insertEls(topDiv, bottomDiv)
+    this.insertEls(topDiv, bottomDiv, topNextDiv)
   }
   
   configure = () => {
